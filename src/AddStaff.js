3f8ff9f604d42a4246210f5864aa1e8b07873fb0
Machiel <machielvanderwalt27@gmail.com>
2023-08-25 21:56:53 +0200

    Add validation to the Add Staff form and fix some bugs

--- src/AddStaff.js
+++ src/AddStaff.js
@@ -3,8 +3,10 @@ import { useNavigate } from 'react-router-dom';
 import { addStaff } from './api';
 import BackArrow from './BackArrow';
 
+
 function AddStaff() {
   const [formData, setFormData] = useState({});
+  const [validated, setValidated] = useState(false); // <-- Add this state
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -15,90 +17,136 @@ function AddStaff() {
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await addStaff(formData);
-      navigate('/');
-    } catch (error) {
-      console.error("Error adding staff:", error);
+    const form = e.currentTarget;
+
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    } else {
+      e.preventDefault();
+      try {
+        await addStaff(formData);
+        navigate('/');
+      } catch (error) {
+        console.error("Error adding staff:", error);
+      }
     }
+
+    setValidated(true);
   };
 
   return (
     <div className="container mt-5">
       <BackArrow />
       <h2 className="mb-4">Add New Staff Member</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input 
-            type="text" 
-            name="name" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="name"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="surname">Surname:</label>
-          <input 
-            type="text" 
-            name="surname" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="surname"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="addressType">Address Type:</label>
-          <input 
-            type="text" 
-            name="addressType" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="addressType"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="streetAddress">Street Address:</label>
-          <input 
-            type="text" 
-            name="streetAddress" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="streetAddress"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="suburb">Suburb:</label>
-          <input 
-            type="text" 
-            name="suburb" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="suburb"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="city">City:</label>
-          <input 
-            type="text" 
-            name="city" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="city"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="postalCode">Postal Code:</label>
-          <input 
-            type="text" 
-            name="postalCode" 
-            onChange={handleChange} 
-            className="form-control" 
-            id="postalCode"
-          />
-        </div>
+      <form onSubmit={handleSubmit} noValidate validated={validated.toString()}>
+      <fieldset>
+          <legend>Personal Details</legend>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name:</label>
+            <input 
+              type="text" 
+              name="name" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="name"
+              required
+            />
+            <div className="invalid-feedback">
+              Please provide a name.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="surname" className="form-label">Surname:</label>
+            <input 
+              type="text" 
+              name="surname" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="surname"
+              required
+            />
+            <div className="invalid-feedback">
+              Please provide a surname.
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>Address</legend>
+          <div className="mb-3">
+            <label htmlFor="addressType" className="form-label">Address Type:</label>
+            <input 
+              type="text" 
+              name="addressType" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="addressType"
+              required
+            />
+            <div className="invalid-feedback">
+              Please provide an address type.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="streetAddress" className="form-label">Street Address:</label>
+            <input 
+              type="text" 
+              name="streetAddress" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="streetAddress"
+              required
+            />
+            <div className="invalid-feedback">
+              Please provide a street address.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="suburb" className="form-label">Suburb:</label>
+            <input 
+              type="text" 
+              name="suburb" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="suburb"
+              required
+            />
+            <div className="invalid-feedback">
+              Please provide a suburb.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="city" className="form-label">City:</label>
+            <input 
+              type="text" 
+              name="city" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="city"
+              required
+            />
+            <div className="invalid-feedback">
+              Please provide a city.
+            </div>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="postalCode" className="form-label">Postal Code:</label>
+            <input 
+              type="text" 
+              name="postalCode" 
+              onChange={handleChange} 
+              className="form-control" 
+              id="postalCode"
+              required
+              pattern="\d{4}"
+            />
+            <div className="invalid-feedback">
+              Please provide a valid postal code.
+            </div>
+          </div>
+        </fieldset>
+        
         <button type="submit" className="btn btn-primary mt-4">Add Staff</button>
       </form>
     </div>
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { getStaff } from './api';
-import { useStaff } from './StaffContext';
-import whatnowLogo from './whatnow_logo.png';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { getStaff } from "./api";
+import { useStaff } from "./StaffContext";
+import whatnowLogo from "./whatnow_logo.png";
 
 function HomePage() {
   const { staffList, setStaffList } = useStaff();
@@ -25,24 +25,50 @@ function HomePage() {
   return (
     <div className="container mt-5">
       <div className="d-flex justify-content-center align-items-center main-logo">
-        <img src={whatnowLogo} alt='whatnow logo' />
+        <img src={whatnowLogo} alt="whatnow logo" />
       </div>
-      <h1 className="mb-4">Welcome to the <i>whatnow</i> Inventory Ordering System</h1>
-      <p>Since we don't have a login system, please don't change anyone else's cart ;)</p>
-      <p>If you can't find your name in the list, please click on the "Add New Staff Member" button below</p>
+      <h1 className="mb-4">
+        Welcome to the <i>whatnow</i> Inventory Ordering System
+      </h1>
+      <p>
+        Since we don't have a login system, please don't change anyone else's
+        cart ;)
+      </p>
+      <p>
+        If you can't find your name in the list, please click on the "Add New
+        Staff Member" button below
+      </p>
       <div className="list-group">
-        {staffList.map(staff => (
-          <div key={staff.staffID} className="list-group-item d-flex justify-content-between align-items-center">
+        {staffList.map((staff) => (
+          <div
+            key={staff.staffID}
+            className="list-group-item d-flex justify-content-between align-items-center"
+          >
             {staff.name} {staff.surname}
             <div className="gridBtns">
-              <Link to={`/staff-details/${staff.staffID}`} className="btn btn-info btn-sm"><i className="material-icons align-middle">perm_identity</i>View Details</Link>
-              <Link to={`/cart/${staff.staffID}`} className="btn btn-primary btn-sm"><i className="material-icons align-middle">shopping_bag</i>View Cart</Link>
+              <Link
+                to={`/staff-details/${staff.staffID}`}
+                className="btn btn-info btn-sm"
+              >
+                <i className="material-icons align-middle">perm_identity</i>View
+                Details
+              </Link>
+              <Link
+                to={`/cart/${staff.staffID}`}
+                className="btn btn-primary btn-sm"
+              >
+                <i className="material-icons align-middle">shopping_bag</i>View
+                Cart
+              </Link>
             </div>
           </div>
         ))}
       </div>
       <div className="mt-4">
-        <Link to="/add-staff" className="btn btn-success"><i className="material-icons align-middle icon-margin">person_add</i>Add New Staff Member</Link>
+        <Link to="/add-staff" className="btn btn-success">
+          <i className="material-icons align-middle icon-margin">person_add</i>
+          Add New Staff Member
+        </Link>
       </div>
     </div>
   );
