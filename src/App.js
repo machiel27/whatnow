7d21ae35dfa0f43d3d2edf328d18389bd3fc97a1
Machiel <machielvanderwalt27@gmail.com>
2023-08-25 21:10:26 +0200

    Update App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import CartPage from './CartPage';
-import StaffDetails from './StaffDetails';
-import { StaffProvider } from './StaffContext';
-import AddStaff from './AddStaff';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import CartPage from "./CartPage";
+import StaffDetails from "./StaffDetails";
+import { StaffProvider } from "./StaffContext";
+import AddStaff from "./AddStaff";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./App.css";
 
 function App() {
   return (
     <StaffProvider>
       <Router>
-        <Routes>
-          <Route path="/cart/:staffId" element={<CartPage />} />
-          <Route path="/staff-details/:staffId" element={<StaffDetails />} />
-          <Route path="/add-staff" element={<AddStaff />} />
-          <Route path="/" element={<HomePage />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/cart/:staffId" element={<CartPage />} />
+            <Route path="/staff-details/:staffId" element={<StaffDetails />} />
+            <Route path="/add-staff" element={<AddStaff />} />
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </main>
+        <Footer />
       </Router>
     </StaffProvider>
   );
 }
 
 export default App;
-
---
+++ src/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-light py-3 mt-5 ">
+      <div className="container text-end p-3">Created by Machiel</div>
+    </footer>
+  );
+}
+
+export default Footer;
