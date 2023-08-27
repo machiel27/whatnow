df2508eb46d178c65b31d466ca6f5599e1668485
Machiel <machielvanderwalt27@gmail.com>
2023-08-27 20:33:34 +0200

    Update code to use async/await syntax for data fetching and add error handling with a modal.

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,30 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { useStaffList } from './useStaffList';
+import { useStaffList } from "./useStaffList";
 import { Link } from "react-router-dom";
 import { useStaff } from "./StaffContext";
 import whatnowLogo from "./whatnow_logo.png";
-import Spinner from './Spinner';
+import Spinner from "./Spinner";
 
 function HomePage() {
   const refetchStaffList = useStaffList();
   const { staffList } = useStaff();
   const [loadingStaff, setLoadingStaff] = useState({});
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     const fetchStaff = async () => {
+      try {
         if (staffList.length === 0) {
-            setLoadingStaff(true);
-            await refetchStaffList();
-            setLoadingStaff(false);
+          setLoadingStaff(true);
+          await refetchStaffList();
+          setLoadingStaff(false);
         } else {
-            setLoadingStaff(false);
+          setLoadingStaff(false);
         }
+      } catch (error) {
+        setLoadingStaff(false);
+        setShowModal(true);
+      }
     };
 
     fetchStaff();
-}, [staffList, refetchStaffList]);
-
-
+  }, [staffList, refetchStaffList]);
 
   return (
     <div className="container mt-5">
@@ -43,34 +47,34 @@ function HomePage() {
         Staff Member" button below
       </p>
       {loadingStaff ? (
-            <Spinner />
-        ) : (
-      <div className="list-group">
-        {staffList.map((staff) => (
-          <div
-            key={staff.staffID}
-            className="list-group-item d-flex justify-content-between align-items-center"
-          >
-            {staff.name} {staff.surname}
-            <div className="gridBtns">
-              <Link
-                to={`/staff-details/${staff.staffID}`}
-                className="btn btn-info btn-sm"
-              >
-                <i className="material-icons align-middle">perm_identity</i>View
-                Details
-              </Link>
-              <Link
-                to={`/cart/${staff.staffID}`}
-                className="btn btn-primary btn-sm"
-              >
-                <i className="material-icons align-middle">shopping_bag</i>View
-                Cart
-              </Link>
+        <Spinner />
+      ) : (
+        <div className="list-group">
+          {staffList.map((staff) => (
+            <div
+              key={staff.staffID}
+              className="list-group-item d-flex justify-content-between align-items-center"
+            >
+              {staff.name} {staff.surname}
+              <div className="gridBtns">
+                <Link
+                  to={`/staff-details/${staff.staffID}`}
+                  className="btn btn-info btn-sm"
+                >
+                  <i className="material-icons align-middle">perm_identity</i>
+                  View Details
+                </Link>
+                <Link
+                  to={`/cart/${staff.staffID}`}
+                  className="btn btn-primary btn-sm"
+                >
+                  <i className="material-icons align-middle">shopping_bag</i>
+                  View Cart
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       )}
       <div className="mt-4">
         <Link to="/add-staff" className="btn btn-success">
@@ -78,6 +82,39 @@ function HomePage() {
           Add New Staff Member
         </Link>
       </div>
+      {showModal && (
+        <div className="modal show d-block" tabIndex="-1">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title">Warning</h5>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setShowModal(false)}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <p>
+                  It seems like my Azure Database Sever is still waking up, you
+                  might have to refresh the page a few times. If the issue
+                  persists please reach out to me.
+                </p>
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  onClick={() => setShowModal(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {showModal && <div className="modal-backdrop show"></div>}
     </div>
   );
 }
