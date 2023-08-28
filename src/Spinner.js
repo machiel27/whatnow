bba9c04d8f87df9db7b371517747c9a1f3b44320
Machiel <machielvanderwalt27@gmail.com>
2023-08-28 09:46:23 +0200

    Added a message to the Spinner component to inform users to refresh the browser tab if the page doesn't load

--- src/Spinner.js
+++ src/Spinner.js
@@ -5,6 +5,7 @@ const Spinner = ({ loading }) => {
     return (
         <div className="spinner-container">
             <ClipLoader size={50} color={"#123abc"} loading={loading} />
+            <p> If this page doesn't load, please refresh the browser tab. The Database might still be waking up. :)</p>
         </div>
     );
 };
